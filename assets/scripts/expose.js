3de2661589b1915a357547c474bc5ee9536b880d
Melvyn <42531205+melvyn9@users.noreply.github.com>
2022-10-26 14:59:26 -0700

    Images changing on Select

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,15 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  var hornSelect = document.getElementById("horn-select");
+
+  hornSelect.onchange = function() {
+    if (hornSelect.value != 'select') {
+      console.log("Changing value");
+      document.getElementsByTagName("img")[0].src=`assets/images/${hornSelect.value}.svg`;
+    }
+    
+  }
+  // var selectedHorn = document.getElementById("horn-select").value;
+
 }

a06b59cec5e0b96930a81c94a004a10375c43107
Vicky <v2chou@ucsd.edu>
2022-10-26 18:53:51 -0700

    converted volume icon

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,7 +6,6 @@ function init() {
   // TODO
   var hornSelect = document.getElementById("horn-select");
   var audio = document.getElementsByTagName("audio")[0];
-  var volume = document.getElementById("volume");
 
   hornSelect.onchange = function () {
     if (hornSelect.value != 'select') {
@@ -20,25 +19,26 @@ function init() {
     audio.play();
   }
 
-
-  // volume.addEventListener("change", function () {
-  //   let volumeControls = document.getElementById("volume-controls");
-  //   let img = volumeControls.getElementsByTagName("img");
-
-  //   if (volume.value === "0") {
-  //     console.log('changing volume')
-  //     img.src = `assets/images/${hornSelect.value}.svg`;
-  //   }
-  // })
-
-
   volume.onchange = function () {
+    let volume = document.getElementById("volume");
     let volumeControls = document.getElementById("volume-controls");
-    let img = volumeControls.getElementsByTagName("img");
+    let img = volumeControls.getElementsByTagName("img")[0];
+    let value = Number(volume.value);
 
-    if (volume.value === "0") {
-      document.getElementById('volume-controls').getElementsByTagName('img').src = `assets/icons/volume-level-${volume.value}.svg`;
-      console.log('changing volume', img.src)
+    if (value === 0) {
+      img.src = `assets/icons/volume-level-${volume.value}.svg`;
+    }
+    else if (value >= 1 && value < 33) {
+      img.src = `assets/icons/volume-level-1.svg`;
+      img.alt = "Volume level 1";
+    }
+    else if (value >= 33 && value < 67) {
+      img.src = `assets/icons/volume-level-2.svg`;
+      img.alt = "Volume level 2";
+    }
+    else {
+      img.src = `assets/icons/volume-level-3.svg`;
+      img.alt = "Volume level 3";
     }
   }
 }
